6aff42e069d6634d9efd9f58d4e8651e332e5a4c
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-18 12:22:41 +0000

    type list

---
+++ lesson/section4-1.py
@@ -0,0 +1,19 @@
+l = [1, 2, 3, 4, 5]
+print(l)
+print('\n')
+print(len(l))
+print(type(l))
+print(list('abcdefg'))
+print('\n')
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(n[::2])
+print(n[::-1])
+print('\n')
+
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)
+print(x[0])
+print(x[0][1])
