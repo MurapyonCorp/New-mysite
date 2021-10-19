f7e7fe851b7e35bc8babdb646a255acee1011bec
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-19 14:20:17 +0000

    type dictionary

---
+++ lesson/section4/4-9.py
@@ -0,0 +1,12 @@
+d = {'x': 10, 'y': 20}
+print(d)
+print(type(d))
+print(d['x'])
+d['x'] = 100
+print(d)
+d['z'] = 200
+print(d)
+d[1] = 10000
+print(d)
+print(dict(a=10, b=20))
+print(dict([('a', 10), ('b', 20)]))
