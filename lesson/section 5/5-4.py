b3a4865b0ad709f8d753ab5cb2d6636a4389be03
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-02 14:26:12 +0900

    compare operator

--- lesson/section 5/5-4.py
+++ lesson/section 5/5-4.py
@@ -1,16 +1,26 @@
-x = 10
+a = 1
+b = 1
 
-if x < 0:
-  print("negative")
-elif x ==0:
-  print("zero")
-else:
-  print("positive")
-  
+# a は b と等しい
+a == b
 
-a = 5
-b = 10
-if a > 0:
-  print("a is positive")
-  if b > 0:
-    print("b is positive")
+# a は b と異なる
+a != b
+
+# a は b よりも小さい
+a < b
+
+# a は b よりも大きい
+a > b
+
+# a は b 以下である
+a <= b
+
+# a は b 以上である
+a >= b
+
+# a も b も真であるならば真
+a > 0 and b > 0
+
+# a または b は真であるならば真
+a > 0 or b > 0
