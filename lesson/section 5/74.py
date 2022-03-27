6b24119ff381ead2d46ba3c9d2341bfd10007d98
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-03-27 14:43:11 +0900

    unit 74

---
+++ lesson/section 5/74.py
@@ -0,0 +1,23 @@
+s = "dghosgheo8058w;oghlalksjngrihg9"
+
+d = {}
+for c in s:
+  if c not in d:
+    d[c] = 0
+  d[c] += 1
+print(d)
+
+d = {}
+for c in s:
+  d.setdefault(c, 0)
+  d[c] += 1
+print(d)
+
+from collections import defaultdict
+
+d = defaultdict(int)
+
+for c in s:
+  d[c] += 1
+print(d)
+print(d['g'])
