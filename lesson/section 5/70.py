393d17ce151bd906bf7191ab3d1d3969b48294f2
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-03-27 15:33:28 +0900

    add

--- lesson/section 5/70.py
+++ lesson/section 5/70.py
@@ -1,5 +1,5 @@
 # from lesson_package.talk import animal
-from lesson_package.talk import *   # *を使った場合はtalkの__init__.pyに__all__に記述が
+from lesson_package.talk import *   # *を使った場合はtalkの__init__.pyに__all__に記述が必要
 
 print(animal.sing())
 print(animal.cry())
