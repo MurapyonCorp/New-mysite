12ec365acc358d00117315a942d4d7050050657f
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-04-13 21:32:20 +0900

    unit 119

--- lesson/section_11/119.py
+++ lesson/section_11/119.py
@@ -23,8 +23,9 @@ with open('New-mysite/lesson/section_11/config.yml', 'w') as yaml_file:
     }
   }, yaml_file, default_flow_style = False)
   
+
 with open('New-mysite/lesson/section_11/config.yml', 'r') as yaml_file:
-  data = yaml.load(yaml_file)
+  data = yaml.load(yaml_file, Loader = yaml.FullLoader)
   print(data, type(data))
   print(data['web_server']['host'])
   print(data['web_server']['port'])
