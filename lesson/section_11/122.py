520037f20d4465cf6f15c06d9dbc412f52987fcc
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-04-14 20:43:15 +0900

    unit 122

---
+++ lesson/section_11/122.py
@@ -0,0 +1,16 @@
+"""
+CRITICAL
+ERROR
+WARNING
+INFO
+DEBUG
+"""
+
+import logging
+
+logging.basicConfig(level = logging.INFO)
+logging.info('info')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.debug('debug')
